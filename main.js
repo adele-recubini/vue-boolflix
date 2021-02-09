ec67262a55084ed31333c49741bc564b9c208eb5
adele <adele.r87@gmail.com>
2021-02-09 13:18:29 +0100

    1- milestone

---
+++ main.js
@@ -0,0 +1,35 @@
+// CHIAVE API
+//
+// 59974366f70c9bc6b02a5ff65f4411d9
+//
+// ESEMPIO DI CHIAMATA
+// https://api.themoviedb.org/3/movie/550?api_key=59974366f70c9bc6b02a5ff65f4411d9
+
+var app = new Vue({
+    el: '#root',
+    data: {
+        risultati: [],
+        ricerca: '',
+
+    },
+    methods: {
+      cerca:function(){
+        const self = this;
+        axios.get('https://api.themoviedb.org/3/search/movie?api_key=59974366f70c9bc6b02a5ff65f4411d9&query=' + self.ricerca)
+          .then((risposta) => {
+            self.risultati = risposta.data.results
+          });
+
+      }
+
+
+
+    }
+
+
+
+
+
+});
+
+Vue.config.devtools = true
