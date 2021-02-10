14a4ead454b75a44723c5aebb4f869f3185e241e
adele <adele.r87@gmail.com>
2021-02-10 11:34:33 +0100

    modifiche

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
-// CHIAVE API
-//
+// CHIAVE API ado
 // 59974366f70c9bc6b02a5ff65f4411d9
-//
 // ESEMPIO DI CHIAMATA
 // https://api.themoviedb.org/3/movie/550?api_key=59974366f70c9bc6b02a5ff65f4411d9
+
+// per 'costruire' la locandina esempio
+// https://image.tmdb.org/t/p    /w342/   wwemzKWzjKYJFfCeiB57q3r4Bcm.png
 
 var app = new Vue({
     el: '#root',
@@ -22,14 +23,26 @@ var app = new Vue({
           });
 
           // serie tv
-      
+
         axios.get('https://api.themoviedb.org/3/search/tv?api_key=59974366f70c9bc6b02a5ff65f4411d9&query=' + self.ricerca)
           .then((risposta) =>{
             self.risultati = risposta.data.results
 
           });
 
-      }
+      },
+
+      // funzione che mi genera la locandina
+
+
+      locandina:function(poster_path){
+        return 'https://image.tmdb.org/t/p' + '/w342/' + poster_path
+      },
+
+      // funzione per la generazione delle stelline
+
+
+
 
 
 // fine methods
