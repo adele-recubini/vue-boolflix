84e97c55fb3828b5f67cb6540fcba937112a25ba
adele <adele.r87@gmail.com>
2021-02-10 10:11:45 +0100

    modifiche

--- main.js
+++ main.js
@@ -13,6 +13,7 @@ var app = new Vue({
 
     },
     methods: {
+      // con una funzione richiamo film e dopo serie tv con lo stesso procedimento, ovviamente cambierà la seconda parte una volta è serch movie e una volta è serach tv
       cerca:function(){
         const self = this;
         axios.get('https://api.themoviedb.org/3/search/movie?api_key=59974366f70c9bc6b02a5ff65f4411d9&query=' + self.ricerca)
@@ -20,16 +21,19 @@ var app = new Vue({
             self.risultati = risposta.data.results
           });
 
+          // serie tv
+      
+        axios.get('https://api.themoviedb.org/3/search/tv?api_key=59974366f70c9bc6b02a5ff65f4411d9&query=' + self.ricerca)
+          .then((risposta) =>{
+            self.risultati = risposta.data.results
+
+          });
+
       }
 
 
-
+// fine methods
     }
-
-
-
-
-
 });
 
 Vue.config.devtools = true
